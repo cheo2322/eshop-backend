775178dd7c4a0554c21352620519312f720f2d72
Sergio Hidalgo-Espinoza <barcecheo2319@gmail.com>
2022-07-15 00:26:01 -0500

    login and security

---
+++ helpers/jwt.js
@@ -0,0 +1,17 @@
+const { expressjwt: jwt } = require("express-jwt");
+
+function authJwt() {
+    const secret = process.env.JWT_SECRET;
+    const api = process.env.API_URL;
+
+    return jwt({
+        secret,
+        algorithms: ['HS256']
+    }).unless({
+        path: [
+            `${api}/users/login`,
+        ]
+    });
+}
+
+module.exports = authJwt;
