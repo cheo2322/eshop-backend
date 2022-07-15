e21ba257a9d73e3ee8d2ac3257f6291c44a79b2a
Sergio Hidalgo-Espinoza <barcecheo2319@gmail.com>
2022-07-15 01:59:25 -0500

    orders api

--- routes/orders.js
+++ routes/orders.js
@@ -4,12 +4,27 @@ const router = express.Router();
 const { OrderItem } = require('../models/order-item');
 
 router.get(`/`, async (req, res) => {
-    const orderList = await Order.find();
+    const orderList = await Order.find().populate('user', 'name').sort({ 'dateOrdered': -1 });
 
     if (!orderList) {
         res.status(500).json({ success: false })
     }
     res.send(orderList);
+});
+
+router.get(`/:id`, async (req, res) => {
+    const order = await Order.findById(req.params.id)
+        .populate('user', 'name')
+        .populate({
+            path: 'orderItems', populate: {
+                path: 'product', populate: 'category'
+            }
+        });
+
+    if (!order) {
+        res.status(500).json({ success: false });
+    }
+    res.send(order);
 });
 
 router.post('/', async (req, res) => {
@@ -29,7 +44,7 @@ router.post('/', async (req, res) => {
     const totalPrices = await Promise.all(orderItemsIdsResolved.map(async (orderItemId) => {
         const orderItem = await OrderItem.findById(orderItemId).populate('product', 'price');
         const totalPrice = orderItem.product.price * orderItem.quantity;
-        return totalPrice
+        return totalPrice;
     }));
 
     const totalPrice = totalPrices.reduce((a, b) => a + b, 0);
@@ -54,5 +69,37 @@ router.post('/', async (req, res) => {
 
     res.send(order);
 })
+
+router.put('/:id', async (req, res) => {
+    const order = await Order.findByIdAndUpdate(
+        req.params.id,
+        {
+            status: req.body.status
+        },
+        { new: true }
+    );
+
+    if (!order)
+        return res.status(400).send('the order cannot be update!');
+
+    res.send(order);
+});
+
+
+router.delete('/:id', (req, res) => {
+    Order.findByIdAndRemove(req.params.id).then(async order => {
+        if (order) {
+            await order.orderItems.map(async orderItem => {
+                await OrderItem.findByIdAndRemove(orderItem);
+            });
+
+            return res.status(200).json({ success: true, message: 'the order is deleted!' });
+        } else {
+            return res.status(404).json({ success: false, message: "order not found!" });
+        }
+    }).catch(err => {
+        return res.status(500).json({ success: false, error: err });
+    });
+});
 
 module.exports = router;
